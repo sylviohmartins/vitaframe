5683ab5685151fcf0f068d9945f0bb4c5b970595
Sylvio Martins <sylviohmartins@gmail.com>
2026-09-13 22:24:49 -0300

    security: use analyzable HTML escaping

--- src/adaptive-interview.mjs
+++ src/adaptive-interview.mjs
@@ -26,11 +26,11 @@ const progress = $('#adaptiveProgress');
 
 function esc(value = '') {
   return String(value)
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
 }
 
 function toast(message) {
