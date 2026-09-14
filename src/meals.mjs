e869a1fe09aaa3e5186afbb64840c44ae0f9f7ad
Sylvio Martins <sylviohmartins@gmail.com>
2026-09-13 22:25:14 -0300

    security: use analyzable HTML escaping

--- src/meals.mjs
+++ src/meals.mjs
@@ -14,11 +14,11 @@ const $ = selector => document.querySelector(selector);
 
 function esc(value = '') {
   return String(value)
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
 }
 
 function id() {
