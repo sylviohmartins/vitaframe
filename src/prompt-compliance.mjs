134072621d08901a85a87718f4508ea18d693be9
Sylvio Martins <sylviohmartins@gmail.com>
2026-09-13 22:25:50 -0300

    security: use analyzable HTML escaping

--- src/prompt-compliance.mjs
+++ src/prompt-compliance.mjs
@@ -12,11 +12,11 @@ let mirror = loadPersisted() || emptyMirror();
 
 function esc(value = '') {
   return String(value)
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
 }
 
 function loadPersisted() {
